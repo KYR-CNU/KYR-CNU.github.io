b4bc2d358ec0e8e950d1ac66e766cb67eb34fe8a
KYR-CNU <rybo1294@gmail.com>
2024-12-20 15:24:52 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function checkAge() {
+    const confirmAccess = confirm("19세 미만은 열람할 수 없습니다. (사유: 폭력성, 공포)\n계속하시겠습니까?");
+    return confirmAccess;
+}
+
 function addReview() {
     const reviewText = document.getElementById("reviewInput").value;
     if (reviewText) {
